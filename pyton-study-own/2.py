509c1d1076f1b20bd096a11c0b094162a7791df6
zhensolid <zhensolid@vip.qq.com>
2022-04-18 15:35:59 +0800

    new

--- pyton-study-own/2.py
+++ pyton-study-own/2.py
@@ -1,5 +1,5 @@
 from faker import Faker
-
 fake = Faker("zh_CN")
-print(fake.profile())
-print(fake.address())
+i = 0
+for i in range(10):
+    print(fake.name(),fake.phone_number(),fake.ssn(min_age=18, max_age=90),fake.address())
