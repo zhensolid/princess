145c257b5afd167ccda3c46d37dfbb4ff9143e98
zhensolid <zhensolid@vip.qq.com>
2022-05-06 16:47:29 +0800

    update

--- pyton-study-own/6.py
+++ pyton-study-own/6.py
@@ -3,18 +3,18 @@ import random
 x = random.randint(1,100)
 counter = 0;
 while True:
-    counter += 1                           #counter = counter + 1
-    try:
-        number = int(input("请输入数字，范围(0-100)"))     #输  入数字必须为数字
+    counter += 1                                         #counter = counter + 1
+    try:                                                 #使用try和except来避免输入异常
+        number = int(input("请输入数字，范围(0-100)"))    #输入数字必须为数字
         if number < x:
             print('输入数字为%d,数字小' % number)
         elif number > x:
             print('输入数字为%d,数字大' % number)
         else:
             print('恭喜你猜对了')
-            break                               #跳出循环
+            break                                        #跳出循环
     except:
-        print('请输入数字')
+        print('请输入数字！！！')
 
 print("你一共才%d" % counter)
 if counter > 7:
