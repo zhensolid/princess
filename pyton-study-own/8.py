dbd98e0b984efd04ad971f8e85f62ea2233faf79
zhensolid <zhensolid@vip.qq.com>
2022-05-11 15:36:55 +0800

    update

---
+++ pyton-study-own/8.py
@@ -0,0 +1,12 @@
+iphone = "123456789"
+for i in iphone:
+    print( f"{i}  "*10 )
+
+
+iphone = "123456789"
+for i in iphone:
+    print( i  * 10 ,end = " ")
+
+iphone = "123456789"
+for i in iphone:
+    print( i  * 10 ,sep = " ")
