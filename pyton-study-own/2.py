e5ff9799ea80874e8e9168bc731a4673650a51d8
zhensolid <zhensolid@vip.qq.com>
2022-06-02 17:45:48 +0800

    update

--- pyton-study-own/2.py
+++ pyton-study-own/2.py
@@ -1,5 +1,5 @@
 from faker import Faker
 fake = Faker("zh_CN")
 i = 0
-for i in range(10):
+for i in range(10): #循环10次 获得名字、电弧号码、身份找、地址
     print(fake.name(),fake.phone_number(),fake.ssn(min_age=18, max_age=90),fake.address())
