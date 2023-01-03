3e82bf9a5d3e18fbac581c25820203ab094225db
无星De夜空 <zhensolid@vip.qq.com>
2023-01-03 17:53:22 +0800

    update

--- get_data.py
+++ get_data.py
@@ -1,18 +1,29 @@
+#注意文件名为：get_data
+
 from openpyxl import load_workbook
 
+
 def get_excel_date():
+#读取excle文件名
     workbook = load_workbook("一季度.xlsx")
+#读取excel内的sheet
     worksheet = workbook["一季度汇总表"]
 
     data = []
+#读取每一行？？？【待理解】
     for row in worksheet.iter_rows(min_row=1):
+        #创建一个名称为dict_data的空白字典
         dict_data = {}
+#读取每一行第一个的数据
         dict_data["name"] = row[0].value
+#读取每一行的第三个数据
+#python从0开始算
         dict_data["value"] = row[2].value
+#将dict_data保存到data
         data.append(dict_data)
+#关闭excel表格
     workbook.close()
-
+#返回data数值
     return data
 # note 输出数据
 # print(get_excel_date())
-
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask,render_template
+#注意必须创建一个templates并将模板保存在其中
+
+from flask import Flask, render_template
+# 之前创建的读取数据文件名
 from get_data import get_excel_date
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     result = get_excel_date()
     # print(result)
     # return render_template("index.html",result=result)
-    return render_template('index.html',result=result)
+    return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
