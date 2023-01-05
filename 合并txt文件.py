5b0971328b1b307b0c6247bd575b17780c9e3db9
无星De夜空 <zhensolid@vip.qq.com>
2023-01-05 11:13:07 +0800

    update

---
+++ 合并txt文件.py
@@ -0,0 +1,20 @@
+import os
+
+# 将当前目录下的所有txt文件合并到一个文件
+
+# 获取当前目录下的文件列表
+file_list = os.listdir()
+
+# 创建一个新文件，用于保存合并后的内容
+with open('result.txt', 'w') as f_result:
+    # 遍历文件列表
+    for filename in file_list:
+        # 只处理txt文件
+        if filename.endswith('.txt'):
+            # 打开txt文件，读取内容
+            with open(filename, 'r',encoding='UTF-8') as f_txt:
+                content = f_txt.read()
+                # 将读取的内容写入到result.txt文件中
+                f_result.write(content)
+                # 写入换行符
+                f_result.write('\n')
